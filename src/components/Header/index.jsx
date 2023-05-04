94892e21977253d309807530818ebf9a82741602
developerabrorbek <abdulkhamidovabrorbek@gmail.com>
2023-05-05 00:24:00 +0500

    mobile header added

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,25 @@
+import SiteLogo from "../../assets/logo.svg";
+import BasketImage from "../../assets/trash.svg";
+import MenuImage from "../../assets/burger.svg";
 
 const Header = () => {
-    return (
-        <div>
-            
+  return (
+    <header>
+      <nav className="nav flex justify-between items-center px-6 py-5">
+        <a href="/" className="site-logo">
+          <img src={SiteLogo} alt="site-logo" />
+        </a>
+        <div className="header-actions flex gap-x-12 items-center">
+          <div className="basket-wrapper">
+            <img src={BasketImage} alt="Basket" />
+          </div>
+          <div className="menu">
+            <img src={MenuImage} alt="" />
+          </div>
         </div>
-    );
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,9 @@
+import Header from "../../components/Header";
+
 const Home = () => {
     return (
         <div>
+            <Header/>
             <h1>Home page</h1>
         </div>
     );
